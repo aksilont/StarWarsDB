ec780181e645b1743a4dc6b2a8b000ce7d3f3a92
aksilont <aksilont@gmail.com>
2021-06-03 20:57:30 +0300

    Initial Commit

---
+++ StarWarsDB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StarWarsDB
+//
+//  Created by Aksilont on 03.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
