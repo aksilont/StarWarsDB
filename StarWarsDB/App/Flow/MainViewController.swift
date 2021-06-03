5a687a544be3397d0fb6448a135e0a726b6f5ef4
aksilont <aksilont@gmail.com>
2021-06-03 21:19:41 +0300

    Первоначальная подготовка проекта

---
+++ StarWarsDB/App/Flow/MainViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MainViewController.swift
+//  StarWarsDB
+//
+//  Created by Aksilont on 03.06.2021.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - UI
+    
+    private func setupUI() {
+        view.backgroundColor = .systemGreen
+    }
+    
+}
