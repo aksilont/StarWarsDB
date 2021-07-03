b5f7b8ac762dc50efc315777a50c81a9025fc589
aksilont <aksilont@gmail.com>
2021-07-03 15:39:47 +0300

    Дорабтка CoreDataManager - другой вариант реализации CoreDataStack, основанный на NSPersistentContainer

--- StarWarsDB/Core/Core Data/CoreDataManager.swift
+++ StarWarsDB/Core/Core Data/CoreDataManager.swift
@@ -7,6 +7,8 @@
 
 import CoreData
 
+// Алтернативный вариант создания CoreDataStack с ипользование Persistent Container
+
 final class CoreDataManager {
     
     let migrator: CoreDataMigratorProtocol
@@ -16,10 +18,32 @@ final class CoreDataManager {
     
     static let shared: CoreDataManager = CoreDataManager()
     
+    // MARK: - CoreData Stack
+    
     lazy var persistentContainer: NSPersistentContainer = {
-       let container = NSPersistentContainer(name: "StarWarsDB")
+        let container = NSPersistentContainer(name: "StarWarsDB")
+        let description = container.persistentStoreDescriptions.first
+        description?.shouldInferMappingModelAutomatically = false
+        description?.shouldMigrateStoreAutomatically = false
+        description?.type = storeType
         return container
     }()
+    
+    lazy var mainContext: NSManagedObjectContext = {
+        let context = persistentContainer.viewContext
+        context.automaticallyMergesChangesFromParent = true
+        return context
+    }()
+    
+    lazy var backgroundContext: NSManagedObjectContext = {
+        let context = persistentContainer.newBackgroundContext()
+        context.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy
+        return context
+    }()
+    
+    func makePrivateContext() -> NSManagedObjectContext {
+        return backgroundContext
+    }
     
     // MARK: - Init
     
@@ -40,12 +64,44 @@ final class CoreDataManager {
     
     private func loadPersistentStore(completion: @escaping() -> Void) {
         migrateStoreIfNeeded {
-            
+            self.persistentContainer.loadPersistentStores { _, error in
+                guard error == nil else { fatalError("Was unable to load store \(error!.localizedDescription)") }
+                completion()
+            }
         }
     }
     
     private func migrateStoreIfNeeded(completion: @escaping() -> Void) {
+        guard let storeUrl = persistentContainer.persistentStoreDescriptions.first?.url else {
+            fatalError("persistentContrainer was not setup properly")
+        }
         
+        if migrator.requiresMigration(at: storeUrl, toVersion: CoreDataMigrationVersion.current) {
+            DispatchQueue.global(qos: .userInitiated).async {
+                self.migrator.migrateStore(at: storeUrl, toVersion: CoreDataMigrationVersion.current)
+                DispatchQueue.main.async {
+                    completion()
+                }
+            }
+        } else {
+            completion()
+        }
+    }
+    
+    // MARK: - Save to store
+    
+    func saveToStore() {
+        guard mainContext.hasChanges else {
+            debugPrint("Data has not changes")
+            return
+        }
+        
+        do {
+            try mainContext.save()
+            debugPrint("Data succesfully saved to store")
+        } catch {
+            debugPrint("Data not saved to store with error \(error.localizedDescription)")
+        }
     }
     
 }
